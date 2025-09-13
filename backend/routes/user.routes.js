837a28855199f75919c7a5dc462b4ccacf35896c
Ramharsh-aidev <ramharshdandekar@gmail.com>
2025-09-13 13:52:42 +0530

    updated proper routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,10 @@
+// backend/routes/user.routes.js
 const router = require('express').Router();
 const { getProfile, updateProfile, deleteAccount } = require('../controllers/user.controller');
 const authenticateToken = require('../middlewares/auth.middleware');
 
-router.get('/:email', authenticateToken, getProfile);
-router.put('/:email', authenticateToken, updateProfile);
-router.delete('/:email', authenticateToken, deleteAccount);
+router.get('/profile', authenticateToken, getProfile);
+router.put('/profile', authenticateToken, updateProfile);
+router.delete('/profile', authenticateToken, deleteAccount);
 
 module.exports = router;
